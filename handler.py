8a94a0eb090036da9b4b7afedcc8d24e334ded4b
farmer0010 <zmqkf16@naver.com>
2026-04-09 13:09:52 +0900

    fix: handler.py 테스트 코드

--- handler.py
+++ handler.py
@@ -1,5 +1,33 @@
-from mangum import Mangum
+import json
+import os
+import boto3
+import socket
 
-from app.main import app
+def handler(event, context):
+    ssm_prefix = os.environ.get('SSM_PREFIX')
+    ssm = boto3.client('ssm')
+    
+    results = {}
+    
+    try:
+        endpoint_res = ssm.get_parameter(Name=f"{ssm_prefix}/DB_ENDPOINT")
+        rds_host = endpoint_res['Parameter']['Value'].split(':')[0]
+        
+        ssm.get_parameter(Name=f"{ssm_prefix}/db_password", WithDecryption=True)
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        s.connect((rds_host, 3306))
+        s.close()
+        
+        results['status'] = "success"
+        results['connected_to'] = rds_host
+        
+    except Exception as e:
+        results['status'] = "fail"
+        results['error'] = str(e)
 
-handler = Mangum(app, lifespan="off")
+    return {
+        'statusCode': 200,
+        'body': json.dumps(results)
+    }
