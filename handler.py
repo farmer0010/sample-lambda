631bcbc946c5ed10edfdc500e59abe1fb1f22c1e
farmer0010 <zmqkf16@naver.com>
2026-04-27 12:17:53 +0900

    fix: handler.py rds 옵션 제거

--- handler.py
+++ handler.py
@@ -1,18 +1,5 @@
-import json
-import socket
+from mangum import Mangum
+from app.main import app
 
-def handler(event, context):
-    host = "juyo-serverless-dev-db.cvcmqeyyopul.ap-northeast-2.rds.amazonaws.com"
-    
-    try:
-        socket.create_connection((host, 3306), timeout=2)
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps({"result": "SUCCESS!"})
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({"result": "FAIL", "error": str(e)})
-        }
+# API Gateway의 요청을 FastAPI(app)로 전달해주는 어댑터입니다.
+handler = Mangum(app)
