f153630a915a96263d9c9e87cd3871965720dc77
farmer0010 <zmqkf16@naver.com>
2026-05-07 09:41:07 +0900

    cli문 작성 진행중

--- cli/mylog.py
+++ cli/mylog.py
@@ -1,44 +1,69 @@
 import argparse
-import urllib.request
-import json
+import requests
 
-API_URL = "https://4roukuec13.execute-api.ap-northeast-2.amazonaws.com/v1/add"
+BASE_URL = "https://4roukuec13.execute-api.ap-northeast-2.amazonaws.com/v1/add"
 
-def send_to_aws(content):
-    payload = {
-        "category" : "cli",
-        "content" : content
-    }
-    json_data = json.dumps(payload).encode("utf-8")
-
-    req = urllib.request.Request(
-        url=API_URL,
-        data=json_data,
-        headers={'content-type': 'application/json'}
-    )
+def add_memo(content: str, category: str):
+    """AWS 서버에 메모를 저장 요청합니다."""
+    url = f"{BASE_URL}/add"
+    payload = {"content": content, "category": category}
 
     try:
-        with urllib.request.urlopen(req) as response:
-            result = json.loads(response.read().decode("utf-8"))
-            print("[aws 저장 성공]")
-            print(f"서버의 응답 메시지: {result['message']}")
-            print(f"저장된 고유 ID: {result['id']}")
-    except Exception as e:
-        print(f"서버 통신중 에러 발생: {e}")
+        response = requests.post(url, json=payload)
+        response.raise_for_status() # 200번대 응답이 아니면 에러처리
+        result = response.json()
+        print(f"> [{category}] 메모가 클라우드에 저장되었습니다. (ID: {result.get('id')})")
+    except requests.exceptions.RequestException as e:
+        print(f"서버와의 통신에 실패했습니다. \n에러:{e}")
 
+def list_memos(category: str, limit: int, search: str):
+    """AWS서버에 메모를 조회하고 요청하고 출력합니다"""
+    url = f"{BASE_URL}/memos"
+
+    params = {"limit": limit}
+    if category:
+        params["category"] = category
+    if search:
+        params["search"] = search
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        memos = response.json.get("memos", [])
+
+        if not memos:
+            print("> 클라우드에 저장된 메모가 없습니다.")
+            return
+
+        print(f"> 최근 글라우드메모 ({len(memos)}개 출력)")
+        for memo in memos:
+            date_str = memo.get('created_at', '')[2:10].replace('-', '.')
+            print(f"  [ID: {memo['id']}] [{memo['category']}] {date_str} | {memo['content']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"메모 조회에 실패했습니다. \n에러: {e}")
 
 def main():
-    parser = argparse.ArgumentParser(description="나만의 터미널 메모장")
-    subparsers = parser.add_subparsers(dest="command", help="사용할 명령어를 입력하세요")
+    parser = argparse.ArgumentParser(description="개발자 전용 CLI 메모장 MyLog (AWS 연동)")
+    subparsers = parser.add_subparsers(dest="command", help="사용할 명령어")
 
-    add_parser = subparsers.add_parser("add", help="새로운 메모를 저장합니다.")
-    add_parser.add_argument("content", type=str, help="저장할 메모 내용을 입력하세요")
+    # 명령어 1. add
+    parser_add = subparsers.add_parser("add", help="새로운 메모를 기록합니다.")
+    parser_add.add_argument("content", type=str, help="저장할 메모 내용")
+    parser_add.add_argument("-c", "--category", type=str, default="basic", help="카테고리 지정 (기본값: basic)")
+
+    # 명령어 2. list
+    parser_list = subparsers.add_parser("list", help="저장된 메모를 조회합니다.")
+    parser_list.add_argument("category", type=str, nargs="?", default=None, help="특정 카테고리만 조회")
+    parser_list.add_argument("-l", "--limit", type=int, default=5, help="출력할 메모 개수")
+    parser_list.add_argument("-s", "--search", type=str, default=None, help="검색어 포함 내용 찾기")
 
     args = parser.parse_args()
 
     if args.command == "add":
-        print(f"입력하신 '{args.content} 문장을 클라우드로 전송'")
-        send_to_aws(args.content)
+        add_memo(args.content, args.category)
+    elif args.command == "list":
+        list_memos(args.category, args.limit, args.search)
     else:
         parser.print_help()
 
